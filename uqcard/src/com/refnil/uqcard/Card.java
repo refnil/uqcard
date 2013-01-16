58ec49ef6e0b1b062665ce9c74bda88385e39e61
DoctorMambo <Fillion.brian@gmail.com>
2013-01-16 11:31:49 -0500

    yo

--- uqcard/src/com/refnil/uqcard/Card.java
+++ uqcard/src/com/refnil/uqcard/Card.java
@@ -1,22 +1,28 @@
 package com.refnil.uqcard;
 
-import android.text.style.ImageSpan;
 import android.widget.Gallery;
 import android.widget.ImageView;
 
-public class Card 
+public class Card extends ImageView
 {
 	private int cost;
 	private String name;
+	private String description;
 	private String flavorText;
 	private ImageView art;
+	private ImageView imageDescription;
+	private ImageView imageName;
 	
 	public Card(int cost, String name)
 	{
+		super(null);
 		this.cost = cost;
 		this.name = name;
 		this.flavorText = "";
-		this.art = new ImageView(null);
+		this.imageDescription = (ImageView) findViewById(R.id.imageView1);
+		
 	}
+	
+	
 	
 }
