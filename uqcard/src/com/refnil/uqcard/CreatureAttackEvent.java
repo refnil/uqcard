766daeae74476da53c110507cd4032eb2cea4f87
DoctorMambo <Fillion.brian@gmail.com>
2013-02-04 16:58:20 -0500

    partie 1 de l<implementation des events dattaques et de selection

---
+++ uqcard/src/com/refnil/uqcard/CreatureAttackEvent.java
@@ -0,0 +1,10 @@
+package com.refnil.uqcard;
+
+public class CreatureAttackEvent extends Event{
+
+	CreatureAttackEvent(Event_Type t) {
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ uqcard/src/com/refnil/uqcard/SelectedCardEvent.java
@@ -0,0 +1,10 @@
+package com.refnil.uqcard;
+
+public class SelectedCardEvent extends Event{
+
+	SelectedCardEvent(Event_Type t) {
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
