e252d234da224fa9767436f6ec5ec395c4a1e5f0
FlyingPanda <c.dallaire93@gmail.com>
2013-05-02 22:03:51 -0400

    liste des cartes faites.

--- uqcard/src/com/refnil/uqcard/view/CardListFragment.java
+++ uqcard/src/com/refnil/uqcard/view/CardListFragment.java
@@ -3,6 +3,11 @@ package com.refnil.uqcard.view;
 import java.util.ArrayList;
 
 import com.refnil.uqcard.R;
+import com.refnil.uqcard.data.CachedCardStore;
+import com.refnil.uqcard.data.CachedCardStore.CachedStoreNotInitialised;
+import com.refnil.uqcard.data.Card;
+import com.refnil.uqcard.data.CreatureCard;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,7 +17,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class CardListFragment extends Fragment {
-	
+	public CachedCardStore cardStore;
 	public CardListFragment() {
 	}
 
@@ -28,9 +33,29 @@ public class CardListFragment extends Fragment {
 	            android.R.layout.simple_list_item_1,
 	            listItems);
 	    
-	    //Temp
-		listItems.add("Small raton 1/1");
-		//End temp
+	    
+	    try{
+	    	cardStore = CachedCardStore.getStore();
+	    	int i=0;
+	    	while(cardStore.getCard(i)!=null)
+	    	{
+	    		Card card = cardStore.getCard(i);
+	    		String info = card.getName().toString() + " " + card.getCost();
+	    		
+	    		if(card instanceof CreatureCard)
+	    		{
+	    			CreatureCard creature = (CreatureCard) card;
+	    			info += " " + String.valueOf(creature.getAtk()) + " " + String.valueOf(creature.getDef()) + " " + String.valueOf(creature.getHp());
+	    		}
+	    		
+	    		listItems.add(info);
+	    		i++;
+	    	}
+	    }catch(CachedStoreNotInitialised e)
+	    {
+	    	// TODO Auto-generated catch block
+	    	e.printStackTrace();
+	    }
 		
 		ListView lv = (ListView) view.findViewById(R.id.listViewCardsList);
 		lv.setAdapter(adapter);
